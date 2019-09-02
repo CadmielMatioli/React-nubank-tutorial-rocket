6d26fc29685238aed1137b29796f7d88386e1255
Cadmiel Matioli Donato <cadmatioli@gmail.com>
2019-09-02 18:04:56 -0300

    Alterado layout completo

--- nubank/src/pages/Main/index.js
+++ nubank/src/pages/Main/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
-import {Container} from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation} from './styles';
 
 import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
 
 export default function Main(){
   return(
     <Container>
-     <Header/>
+    <Header/>
+        <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666"/>
+            <Icon name="visibility-off" size={28} color="#666"/>
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo Disponível</Title>
+            <Description>R$ 190.530,91</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$20,00 recebida de Silvio Capelo hoje às 06:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+    <Tabs/>
     </Container>
-  );
-}
+    );
+  }
